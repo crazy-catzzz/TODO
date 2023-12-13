73630a8a7e985cbe2e3ac46dcbce44ea1b1689c6
Davide Guerrazzi <davide.guerrazzi@protonmail.com>
2023-12-14 00:16:12 +0100

    App POST

--- index.ts
+++ index.ts
@@ -2,6 +2,26 @@ import express from "express";
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Endpoint POST /api/v1/user
+app.post("/api/v1/user", (req, res) => {
+    /*
+    il body della richiesta di creazione utente è circa così:
+    {
+        "username": "user",
+        "password": "pass"
+    }
+    */
+
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    
+})
+
 // Listen on port
 const port = 8080;
 app.listen(port, () => {

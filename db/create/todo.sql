4098ded95657acd287a10463837c851a617e804c
Davide Guerrazzi <davide.guerrazzi@protonmail.com>
2023-12-14 09:33:39 +0100

    Ristrutturazione DB

--- db/create/todo.sql
+++ db/create/todo.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (
     id integer PRIMARY KEY AUTOINCREMENT,
     username text NOT NULL,
-    password text NOT NULL,
-    creation_date datetime NOT NULL
+    password_hash text NOT NULL,
+    creation_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE lists (
     id integer PRIMARY KEY AUTOINCREMENT,
     owner_id integer NOT NULL,
     list_name text NOT NULL,
-    creation_date datetime NOT NULL,
-    last_modified datetime NOT NULL,
+    creation_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (owner_id) REFERENCES users(id)
 );
@@ -18,8 +18,8 @@ CREATE TABLE todos (
     list_id integer,
     todo_name text NOT NULL,
     completion_status integer DEFAULT 0 NOT NULL,
-    creation_date datetime NOT NULL,
-    last_modified datetime NOT NULL,
+    creation_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     
     PRIMARY KEY (id, list_id),
     FOREIGN KEY (list_id) REFERENCES lists(id)

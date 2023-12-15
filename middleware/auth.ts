59fc95b29b591a1835005233336508c700dd94d2
Davide Guerrazzi <davide.guerrazzi@protonmail.com>
2023-12-15 11:27:21 +0100

    Added authorization middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+// Middleware per autneticazione via token
+
+export function authenticate(req : Request, res : Response, next : any) : void {
+    const auth_header = req.headers["authorization"];
+    const token = auth_header && auth_header.split(' ')[1];
+
+    if (token == null) {
+        res.sendStatus(401); // 401 Unauthorized
+        return;
+    }
+
+    jwt.verify(token, Bun.env.TOKEN_SECRET!, (err : any, user : any) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(403); // 403 Forbidden
+            return;
+        }
+
+        req.body.user = user;
+
+        next();
+    });
+}

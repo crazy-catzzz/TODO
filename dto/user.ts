ceba176d017dc27a1b3616af1f5add7f32f4a181
Davide Guerrazzi <davide.guerrazzi@protonmail.com>
2024-01-11 09:15:03 +0100

    Aggiunta proprietà

--- dto/user.ts
+++ dto/user.ts
@@ -2,17 +2,20 @@ export class User {
     id : number;
     username : string;
     creation_date : string;
+    password_hash : string;
     permission_level : number;
 
     constructor(
         id : number = 0,
         username : string = "",
         creation_date : string = "",
+        password_hash : string = "",
         permission_level : number = 0
     ) {
         this.id = id;
         this.username = username;
         this.creation_date = creation_date;
+        this.password_hash = password_hash;
         this.permission_level = permission_level;
     }
 }
